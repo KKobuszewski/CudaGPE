2963ff94ab12cc53288ae91f5fe18192469b8a1f
KKobuszewski <konrad.kobuszewski93@gmail.com>
2015-07-21 16:17:35 +0200

    problems with exapmle shared memory, by allocation of shared memory seems to work fine

--- tests/Shared Mem/shared_mem.cu
+++ tests/Shared Mem/shared_mem.cu
@@ -7,7 +7,7 @@
 __global__ void staticReverse(cuDoubleComplex *d, int n)
 {
   __shared__ cuDoubleComplex s[64];
-  int t = threadIdx.x;
+  int t = threadIdx.x + blockIdx.x*blockDim.x;
   int tr = n-t-1;
   s[t] = d[t];
   __syncthreads();
@@ -17,8 +17,9 @@ __global__ void staticReverse(cuDoubleComplex *d, int n)
 __global__ void dynamicReverse(cuDoubleComplex *d, int n)
 {
   extern __shared__ cuDoubleComplex s[];
-  int t = threadIdx.x;
+  int t = threadIdx.x + blockIdx.x*blockDim.x;
   int tr = n-t-1;
+  printf("tr: %d",tr);
   s[t] = d[t];
   __syncthreads();
   d[t] = s[tr];
@@ -26,36 +27,37 @@ __global__ void dynamicReverse(cuDoubleComplex *d, int n)
 
 int main(void)
 {
-  const int n = 64;
+  const int n = 64*512; // 2**15
   cuDoubleComplex a[n], r[n], d[n];
   
   for (int i = 0; i < n; i++) {
-    a[i] = i;
-    r[i] = n-i-1;
-    d[i] = 0;
+    a[i] = make_cuDoubleComplex( (double) i, 0. );
+    r[i] = make_cuDoubleComplex( (double) n-i-1, 0. );
+    d[i] = make_cuDoubleComplex( 0. , 0.);
   }
   
   cuDoubleComplex *d_d;
   cudaMalloc(&d_d, n * sizeof(cuDoubleComplex)); 
   
+  printf("static reverse\n");
   // run version with static shared memory
   cudaMemcpy(d_d, a, n*sizeof(cuDoubleComplex), cudaMemcpyHostToDevice);
-  staticReverse<<<1,n>>>(d_d, n);
+  staticReverse<<<512,n/512>>>(d_d, n);
   cudaMemcpy(d, d_d, n*sizeof(cuDoubleComplex), cudaMemcpyDeviceToHost);
   for (int i = 0; i < n; i++) 
-    if (d[i] != r[i]) printf("Error: d[%d]!=r[%d] (%d, %d)n", i, i, d[i], r[i]);
+    if ( cuCreal(d[i]) != cuCreal(r[i]) ) printf("Error: d[%d]!=r[%d] (%lf, %lf)n", i, i,cuCreal(d[i]), cuCreal(r[i])); 
   
+  printf("dynamic reverse\n");
   // run dynamic shared memory version
   cudaMemcpy(d_d, a, n*sizeof(cuDoubleComplex), cudaMemcpyHostToDevice);
   
   // WYTESTOWAC odpalenie tego kernela z parametrami dimGrid(512,1,1)/dimBlocks(128,1,1)/3KB = 3*(1<<10) KB Shared Mem per thread !!!
   printf("sizeof cuDoubleComplex: %d",sizeof(cuDoubleComplex));
   printf("memory needed: %d",n*sizeof(cuDoubleComplex));
-  dynamicReverse<<<1,n,n*sizeof(cuDoubleComplex)>>>(d_d, n);
-  
+  dynamicReverse<<<512,n/512,n*sizeof(cuDoubleComplex)/512>>>(d_d, n); 
   
   
   cudaMemcpy(d, d_d, n * sizeof(cuDoubleComplex), cudaMemcpyDeviceToHost);
   for (int i = 0; i < n; i++) 
-    if (d[i] != r[i]) printf("Error: d[%d]!=r[%d] (%d, %d)n", i, i, d[i], r[i]);
+    if ( cuCreal(d[i]) != cuCreal(r[i]) ) printf("Error: d[%d]!=r[%d] (%lf, %lf)n", i, i,cuCreal(d[i]), cuCreal(r[i]));
 }
